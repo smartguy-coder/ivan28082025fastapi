70f3593d2296fbcaee5e7ba09917cb2416f1598a
Vasyl <vasyl@example.com>
2025-10-29 20:29:02 +0200

    crud 2

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import json
+from pathlib import Path
 
 app = FastAPI(
     debug=True
 )
 
-
-class NewBook(BaseModel):
+class Book(BaseModel):
     id: int
     title: str
     author: str
@@ -15,6 +16,27 @@ class NewBook(BaseModel):
     description: str
 
 
+class Storage:
+
+    def __init__(self, filename: str):
+        self.filename = f"{filename}.json"
+
+        storage_file = Path(self.filename)
+        if not storage_file.is_file():
+            with open(self.filename, mode='w', encoding='utf-8') as file:
+                json.dump([], file, indent=4)
+
+    def create(self, book: Book):
+
+        with open(self.filename, mode='r', encoding='utf-8') as file:
+            content: list[dict] = json.load(file)
+
+        content.append(book.__dict__)
+        with open(self.filename, mode='w', encoding='utf-8') as file:
+            json.dump(content, file, indent=4)
+
+
+book_storage = Storage('fiction')
 
 
 @app.get("/")
@@ -23,6 +45,7 @@ def get_info():
 
 
 @app.post("/create")
-def create_book(new_book: NewBook) -> NewBook:
-    print(new_book)
+def create_book(new_book: Book) -> Book:
+    book_storage.create(new_book)
+
     return new_book
