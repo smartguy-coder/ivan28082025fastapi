0c5d4819b1c546974f800a823f9fa1d44d01d0d6
Vasyl <vasyl@example.com>
2025-10-29 20:04:16 +0200

    crud 1

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(
     debug=True
 )
 
 
+class NewBook(BaseModel):
+    id: int
+    title: str
+    author: str
+    price: int
+    cover: str
+    description: str
+
+
+
+
 @app.get("/")
 def get_info():
-    return {"status": "ok"}
+    return {"status": "okm,hfvgjhgfh"}
+
+
+@app.post("/create")
+def create_book(new_book: NewBook) -> NewBook:
+    print(new_book)
+    return new_book
