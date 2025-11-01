c1e86372169000cab73183fe59eb5909cdf0bbb5
Vasyl <vasyl@example.com>
2025-11-01 12:35:00 +0200

    crud 3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 import json
 from pathlib import Path
@@ -33,7 +33,18 @@ class Storage:
 
         content.append(book.__dict__)
         with open(self.filename, mode='w', encoding='utf-8') as file:
-            json.dump(content, file, indent=4)
+            json.dump(content, file, indent=4, ensure_ascii=False )
+
+    def get_book(self, book_id: int) -> dict:
+        with open(self.filename, mode='r', encoding='utf-8') as file:
+            books: list[dict] = json.load(file)
+
+        for book in books:
+            if book['id'] == book_id:
+                return book
+
+        raise HTTPException(detail=f'Book id #{book_id} not found', status_code=status.HTTP_404_NOT_FOUND)
+
 
 
 book_storage = Storage('fiction')
@@ -47,5 +58,11 @@ def get_info():
 @app.post("/create")
 def create_book(new_book: Book) -> Book:
     book_storage.create(new_book)
-
     return new_book
+
+
+@app.get("/{book_id}")
+def get_book(book_id: int) -> Book:
+    book = book_storage.get_book(book_id)
+    return book
+
